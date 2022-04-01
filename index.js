c54bc3c7d491fc585334b257198ac96716ed282c
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-04-01 10:15:25 -0300

    commit exercise

---
+++ index.js
@@ -0,0 +1,22 @@
+const almoco = document.getElementById("almoco");
+const porcentagem = document.getElementById("porcentagem");
+const button = document.getElementById("button");
+const valorTotal = document.getElementById("valor-total");
+
+//Calcular porcentagem
+const calcularPorcentagem = () => {
+  const valorDoAlmoço = Number(almoco.value);
+  const valorDaGorjeta = Number(porcentagem.value) / 100;
+  const totalDaConta = valorDoAlmoço + valorDoAlmoço * valorDaGorjeta;
+  valorTotal.innerText = `Valor Total: R$ ${totalDaConta
+    .toFixed(2)
+    .replace(".", ",")} Reais`;
+    if (totalDaConta == NaN){
+        console.log()
+    }
+
+  // console.log(totalDaConta);
+};
+
+//Evento de click
+button.addEventListener("click", calcularPorcentagem);
