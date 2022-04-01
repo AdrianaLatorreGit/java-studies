0ee3926e366cd57bf6170ffbac7dbe98bdee569a
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-04-01 19:22:52 -0300

    commit prettier

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const valorTotal = document.getElementById("valor-total");
 
 //Calcular porcentagem
 const calcularPorcentagem = () => {
-  const valorDoAlmoço = Number(almoco.value);
-  const valorDaGorjeta = Number(porcentagem.value) / 100;
-  const totalDaConta = valorDoAlmoço + valorDoAlmoço * valorDaGorjeta;
-  valorTotal.innerText = `Valor Total: R$ ${totalDaConta
-    .toFixed(2)
-    .replace(".", ",")} Reais`;
-    if (totalDaConta == NaN){
-        console.log()
+    const valorDoAlmoço = Number(almoco.value);
+    const valorDaGorjeta = Number(porcentagem.value) / 100;
+    const totalDaConta = valorDoAlmoço + valorDoAlmoço * valorDaGorjeta;
+    valorTotal.innerText = `Valor Total: R$ ${totalDaConta
+        .toFixed(2)
+        .replace(".", ",")} Reais`;
+    if (totalDaConta == NaN) {
+        console.log();
     }
 
-  // console.log(totalDaConta);
+    // console.log(totalDaConta);
 };
 
 //Evento de click
